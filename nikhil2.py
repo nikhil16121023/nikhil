543c7b0126aff613642765047b0a5bb935387e55
nikhil16121023 <53078185+nikhil16121023@users.noreply.github.com>
2019-07-19 15:39:21 +0530

    Update nikhil2.py

--- nikhil2.py
+++ nikhil2.py
@@ -1,7 +1,7 @@
-n=float(input("enter a number:"))
-if n%2 = 0:
+n=int(input("enter a number:"))
+if n % 2 = 0:
       print("even")
-elif n%2 == 0:
+elif n % 2 = 1:
       print("odd")
 else:
       print("invalid")
